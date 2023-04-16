f787204c3326ea9b3db3f09a9c54d1021dd577be
Félix Vicente <felixvicent1306@gmail.com>
2023-04-16 15:07:44 -0300

    :sparkles: add user already exists verification

--- src/app/useCases/users/registerUser.ts
+++ src/app/useCases/users/registerUser.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
-import { z } from "zod";
 import bcrypt from "bcrypt";
 import { RegisterUserInput } from "../../../validations/registerUserValidation";
 
@@ -9,6 +8,12 @@ const prisma = new PrismaClient();
 export async function registerUser(request: Request, response: Response) {
   try {
     const { name, email, password }: RegisterUserInput = request.body;
+
+    const userAlreadyExists = await prisma.user.findFirst({ where: { email } });
+
+    if (userAlreadyExists) {
+      return response.status(400).json({ error: "User already exists" });
+    }
 
     const passwordHash = bcrypt.hashSync(password, 8);
 
